a1a8122381c7628f19aa0b577083fa0eec5fa37e
Veri Ferdiansyah <veri_ferdi@outlook.com>
2024-05-12 07:55:39 +0700

    feat: add users table migration

---
+++ src/migrations/1715359754143-create-users-table.ts
@@ -0,0 +1,63 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsersTable1715359754143 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const usersTable = new Table({
+      name: 'users',
+      columns: [
+        {
+          name: 'id',
+          type: 'int',
+          generationStrategy: 'increment',
+          isPrimary: true,
+        },
+        {
+          name: 'firstName',
+          type: 'varchar',
+          isNullable: false,
+        },
+        {
+          name: 'lastName',
+          type: 'varchar',
+          isNullable: false,
+        },
+        {
+          name: 'dateOfBirth',
+          type: 'date',
+          isNullable: false,
+        },
+        {
+          name: 'location',
+          type: 'varchar',
+          isNullable: false,
+        },
+        {
+          name: 'timezone',
+          type: 'varchar',
+          isNullable: false,
+        },
+        {
+          name: 'createdAt',
+          type: 'timestamp',
+          isNullable: false,
+          default: 'CURRENT_TIMESTAMP',
+        },
+        {
+          name: 'updatedAt',
+          type: 'timestamp',
+          isNullable: false,
+          default: 'CURRENT_TIMESTAMP',
+        },
+        {
+          name: 'deletedAt',
+          type: 'timestamp',
+        },
+      ],
+    });
+    await queryRunner.createTable(usersTable, true);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
